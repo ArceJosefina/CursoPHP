bd1133206fe3fc1395c49fed27a50b4e871fabdf
ArceJosefina <arce.josefina.m@gmail.com>
2023-03-07 20:28:11 -0300

    act

---
+++ Curso React/codigo.js
@@ -0,0 +1,36 @@
+//interacciones
+//*comentarios* */
+
+alert("Hola Nerd");
+
+//*permite al usuario ingresar su nombre* */
+prompt("Ingresa tu nombre");
+
+//*confirmacion de la aleta* */
+confirm("Hola, confirma");
+
+//let
+let normbre= "Jose";
+let edad= 37;
+let numero_favorito="13"//upper camel case
+let numeroFavorito="13";//Lowel Camel Case
+let estudia= true;//booleano - BOOLEAN
+let materias= ["JavaScript","Node","12"];//arreglo/matriz - ARRAY
+let finalizado= null;
+estudia="no"
+let numeroFAvorito="14";//Case sensitive
+
+//console.log(materias);
+//document.write(edad + numero_favorito)
+
+//document.write(parseInt(numeroFavorito)+ edad)
+//document.write(Number(numeroFavorito)+ edad)
+
+let nombre2 = prompt("Decimme tu nombre");
+document.write("hola " + nombre2 + " como estas?"+ "<br>");
+alert("hola " + nombre2 + " Estas en mi pagina");
+
+let num2= Number(prompt("Decime un numero"));
+let num2= Number(prompt("Decime otro numero"));
+
+document.write("el resultado de tu suma es:"+(num1+num2));
